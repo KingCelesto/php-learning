ace0bdf698ac0726c14290b2908cb0f7fc170868
David Charles <kingdavidchris01@gmail.com>
2024-09-02 12:09:03 +0100

    added materialize

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Learning</title>
+    <link rel="stylesheet" href="css/materialize.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <h1 class="red-text" >Hello i'm learning php</h1>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/materialize.js"></script>
+</body>
+</html>
